50404bbedea5b1eb94445b96c88b937adc45eb04
doktor_justice <somiandras@gmail.com>
2016-10-28 15:20:01 +0200

    Fix allow origin header for http://dotkomblog.com

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
-  res.set('Access-Control-Allow-Origin', 'https://dotkomblog.com');
+  res.set('Access-Control-Allow-Origin', 'http://dotkomblog.com');
   next();
 });
 app.use('/', routes);
